91ea1e2255cb3e33329c980ed7f2030f9322bc79
H-ymt <y.handai1272@gmail.com>
2023-12-09 18:46:50 +0900

    feat: add excerpt

--- src/lib/newt.ts
+++ src/lib/newt.ts
@@ -2,12 +2,13 @@ import { createClient } from "newt-client-js";
 
 export interface Article {
   title: string;
+  excerpt: string;
   slug: string;
   body: string;
   coverImage: {
     src: string;
   };
-  tags: [];
+  tags: []
 }
 
 export const newtClient = createClient({
